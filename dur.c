c962ce28a1b814d527f8ed34fed205b1dbec39f6
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2013-11-27 01:48:56 +0530

    Structure basically in place and functions defined..now to actually calculate the difference using the year and month modules

--- dur.c
+++ dur.c
@@ -3,17 +3,30 @@
 
 #include<stdio.h>
 #include<string.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include"to_int.h"
 
-main(int argc, char* argv[])
+int d=0;        //to store the output,ie, difference in days
+
+//Make a structure for the date
+struct date
+{
+	int year;
+	int month;
+	int day;
+}d1,d2;
+
+//Make a macro for checking leap year
+#define is_leap(year) (((year%4==0) && (year%100)!=0)||(year%400)==0)
+
+void main(int argc, char* argv[])
 {
         if(argc != 3 ) {
                 printf("Invalid number of arguments\n");
                 exit(1);
         }
 	
-	//Extract the year,monthe,day components from the date
+	//Extract the year,month,day components from the date
 
 	char yi[5],yf[5],mi[3],mf[3],di[3],df[3];	//size+1 for null character at the end
 	
@@ -30,17 +43,6 @@ main(int argc, char* argv[])
 	yf[4]='\0';     //insert null character at the end
         mf[2]='\0';
 	
-	printf("%s %s %s %s %s %s\n",yi,mi,di,yf,mf,df);
-
-	//Make a structure for the date
-	
-	struct date
-	{
-		int year;
-		int month;
-		int day;
-	}d1,d2;
-
 
 	//Convert the string date components to int and assign them to the structure elements
 
@@ -51,11 +53,9 @@ main(int argc, char* argv[])
         d2.month=to_int(mf);
 	d2.day=to_int(df);
 
-        printf("%d %d %d %d %d %d\n",d1.year,d1.month,d1.day,d2.year,d2.month,d2.day);
-
-
-        //store difference b/w the date elements
-        int dy=d2.day-d1.day;
+        
+	//store difference b/w the date elements
+        int dy=d2.year-d1.year;
         int dm=d2.month-d1.month;
         int dd=d2.day-d1.day;
 
@@ -63,11 +63,10 @@ main(int argc, char* argv[])
 	//*Now the date part
 
 
-	//int flag=0;	//to check for wrong dates
-	int d=0;	//to store the output, ie difference in days
+	//int flag=0;	//to check for wrong dates: currently the program is non-interactive so its not being implemented yet
 
 
-	//Procedure :find diff in year -> find diff in month -> find diff in days
+	//Procedure:find diff in year -> find diff in month -> find diff in days
 
 	//diff in year: find diff in year -> diff in month -> diff in days
 	//diff in months -> find diff in months -> diff in days	
@@ -75,25 +74,50 @@ main(int argc, char* argv[])
 
 	if(!dy)	//Year same
 	{
-		//diff_month(d2.month,d1.month);	//Check difference in months
+		//Check difference in months
+		if(!dm)	//Month same
+		{
+			d=dd;	//output=diff in days
+			//done
+		}
+		else
+		{
+			//diff_month();
+		}
 	}
 	else 		//There is Difference in Years
 	{
-		//diff_year(d2.year,d1.year);		//Check difference
+		//diff_year();		//Check difference
 		
 	}
 
+	printf("Difference = %d day(s)\n",d);
 
+}
 
-	int i;
-	char *ptr;
-	ptr=argv[1];
+int check_days(int year,int month)	//find the no of days in a month
+{
+	int leap=is_leap(year);
+	//if(((year%4==0) && (year%100)!=0)||(year%400)==0)
+	//	leap=1;
 	
-	while (*ptr != '\0')
+	switch(month)
 	{
-	//	printf("%c",*ptr);
-		ptr++;
+		case 1:case 3: case 5:case 7: case 8: case 10: case 12:
+			return 31;
+
+		case 4: case 6: case 9:case 11:
+			return 30;
+
+		case 2:
+		if(leap)
+			return 29;
+		else
+			return 28;
 	}
 }
 
+void diff_month()
+{
 
+}	
